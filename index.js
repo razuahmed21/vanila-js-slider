968bab0c286b0fba061ef23de2340c8abaedb7c2
razuahmed21 <razu.exe@gmail.com>
2022-09-25 15:15:18 +0600

    100vh height

--- index.js
+++ index.js
@@ -5,17 +5,6 @@ const totalSlides = slides.length;
 document.getElementById("prev").addEventListener("click", movePrevious);
 document.getElementById("next").addEventListener("click", moveNext);
 
-let defaultText = (document.getElementById("one").textContent =
-	"I believe in God");
-
-function text() {
-	let firstSlideText = (document.getElementById("one").textContent =
-		"I believe in God");
-	let seconfSlideText = (document.getElementById("two").textContent =
-		"Only I spell it Nature");
-	let thirdSlideText = (document.getElementById("three").textContent =
-		"Choose only one master");
-}
 function hideshowSildes() {
 	for (i = 0; i < slides.length; i++) {
 		slides[i].classList.remove("slider-visible");
@@ -25,7 +14,6 @@ function hideshowSildes() {
 
 function movePrevious() {
 	hideshowSildes();
-	text();
 	if (sliderPosition === 0) {
 		sliderPosition = totalSlides - 1;
 	} else {
@@ -36,7 +24,6 @@ function movePrevious() {
 
 function moveNext() {
 	hideshowSildes();
-	text();
 	if (sliderPosition === totalSlides - 1) {
 		sliderPosition = 0;
 	} else {
